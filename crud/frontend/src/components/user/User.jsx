d2360d92566ad93d4611ecc73da12cd86ab03329
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-05-07 03:53:37 -0300

    Update User.jsx

--- crud/frontend/src/components/user/User.jsx
+++ crud/frontend/src/components/user/User.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios'
 
 import Main from '../templates/Main'
 
@@ -8,7 +9,41 @@ const headerProps = {
     subtitle: 'Cadastro de usuário: Adicionar, Listar, Modificar, Excluir'
 }
 
+const baseUrl = "http://localhost:3001/users"
+const initialState = {
+    user: { name: "", email: "" },
+    list: []
+}
+
 export default class User extends Component {
+    state = { ...initialState }
+
+    clear() {
+        this.setState({
+            user: initialState.user
+        })
+    }
+
+    getUpdatedList(user) {
+        const list = this.state.list.filter(usr => usr.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? "put" : "post"
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(response => {
+                const list = this.getUpdatedList(response.data)
+                this.setState({
+                    user: initialState,
+                    list
+                })
+            })
+    }
+
     render() {
         return (
             <Main {...headerProps}>
