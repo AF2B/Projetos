6a06fb98aac0992cbc5e7cf6b9e4afa4b4bc6b71
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-05-07 17:23:26 -0300

    Update User.jsx

--- crud/frontend/src/components/user/User.jsx
+++ crud/frontend/src/components/user/User.jsx
@@ -18,6 +18,15 @@ const initialState = {
 export default class User extends Component {
     state = { ...initialState }
 
+    constructor(props) {
+        super(props)
+        this.clear = this.clear.bind(this)
+        this.getUpdatedList = this.getUpdatedList.bind(this)
+        this.updateField = this.updateField.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.save = this.save.bind(this)
+    }
+
     clear() {
         this.setState({
             user: initialState.user
@@ -28,6 +37,50 @@ export default class User extends Component {
         const list = this.state.list.filter(usr => usr.id !== user.id)
         list.unshift(user)
         return list
+    }
+
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input type="text" className="form-control"
+                                name="name" value={this.state.user.name}
+                                onChange={this.updateField}
+                                placeholder="nome" />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control"
+                                name="email" value={this.state.user.email}
+                                onChange={this.updateField}
+                                placeholder="e-mail" />
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary" onClick={this.save}>
+                            Salvar
+                        </button>
+                        <button className="btn btn-secondary ml-2" onClick={this.clear}>
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
     }
 
     save() {
@@ -47,7 +100,7 @@ export default class User extends Component {
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro...
+                {this.renderForm()}
             </Main>
         )
     }
