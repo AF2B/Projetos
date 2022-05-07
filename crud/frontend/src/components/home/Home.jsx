3a2de5b355df41bc9e4c19f54093f749d3dcdcdb
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-05-06 23:12:25 -0300

    Create Home.jsx

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from "../templates/Main";
+
+export default props =>
+    <Main icon="home" title="Início">
+        <div className="display-4">Teste...</div>
+        <hr />
+        <p className="mb-0">Construção de cadastro desenvolvido em React.</p>
+    </Main>
