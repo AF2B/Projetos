cb58e69d8d7bb0819664e5d47c36cedd50bcce30
André Filipe <dev_andre.filipe@outlook.com>
2022-05-09 17:40:19 -0300

    Create Header.jsx

---
+++ startednextjs/src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(props) {
+    return (
+        <div>
+            <h1>{props.titulo}</h1>
+            <br />
+            <p>Lorem ipsum dolor...</p>
+        </div>
+    )
+}
