aa9248c1235e0863b611bf0da1cc2ca0546ea38b
André Filipe <dev_andre.filipe@outlook.com>
2022-05-09 14:37:10 -0300

    Update Home.jsx

--- crud/frontend/src/components/home/Home.jsx
+++ crud/frontend/src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import Main from "../templates/Main";
 
 export default props =>
     <Main icon="home" title="Início">
-        <div className="display-4">Teste...</div>
+        <div className="display-4">Bem vindo!</div>
         <hr />
         <p className="mb-0">Construção de cadastro desenvolvido em React.</p>
     </Main>
