7bcd98b1521d3654d9eadd11715864487537fc21
André Filipe <dev_andre.filipe@outlook.com>
2022-05-10 05:32:39 -0300

    Update index.tsx

--- crud-next/src/pages/index.tsx
+++ crud-next/src/pages/index.tsx
@@ -1,6 +1,22 @@
 import Layout from "../components/Layout";
+import Table from "../components/Table";
+import Client from "../core/Client";
 
 export default function Home() {
+    const client = [
+        new Client('Ana', 22, '1'),
+        new Client('Alícia', 23, '2'),
+        new Client('Eduarda', 21, '3')
+    ]
+
+    function selectedClient(client: Client) {
+
+    }
+
+    function deleteClient(client: Client) {
+
+    }
+
     return (
         <div className="flex 
         justify-center 
@@ -10,7 +26,7 @@ export default function Home() {
         from-black to bg-gray-500">
 
             <Layout title="Cadastro">
-                <span>Teste...</span>
+                <Table client={client} selectedClient={selectedClient} deleteClient={deleteClient}/>
             </Layout>
         </div>
     )
