3fe9d6008dc0244ec57fd844a938e5ecb2c26dbe
André Filipe <dev_andre.filipe@outlook.com>
2022-05-17 04:15:51 -0300

    Update LibrarianForm.java

--- Biblioteca-Swing/Biblioteca/src/components/addLibrarianForm/LibrarianForm.java
+++ Biblioteca-Swing/Biblioteca/src/components/addLibrarianForm/LibrarianForm.java
@@ -8,19 +8,21 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 import components.adminSection.AdminSection;
+import repository.Librarian;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
 
 public class LibrarianForm {
 	private JFrame librarianForm;
-	private JTextField textField;
-	private JPasswordField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	private JTextField nameTextField;
+	private JPasswordField passwordTextField;
+	private JTextField emailTextField;
+	private JTextField addressTextField;
+	private JTextField cityTextField;
+	private JTextField contactTextField;
 
 	/**
 	 * Launch the application.
@@ -55,68 +57,100 @@ public class LibrarianForm {
 		librarianForm.setBounds(350, 350, 700, 500);
 		librarianForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		textField = new JTextField();
-		textField.setBounds(282, 47, 156, 19);
-		librarianForm.getContentPane().add(textField);
-		textField.setColumns(10);
+		nameTextField = new JTextField();
+		nameTextField.setBounds(282, 47, 156, 19);
+		librarianForm.getContentPane().add(nameTextField);
+		nameTextField.setColumns(10);
 		
-		textField_1 = new JPasswordField();
-		textField_1.setBounds(282, 82, 156, 19);
-		librarianForm.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
+		passwordTextField = new JPasswordField();
+		passwordTextField.setBounds(282, 82, 156, 19);
+		librarianForm.getContentPane().add(passwordTextField);
+		passwordTextField.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(282, 117, 156, 19);
-		librarianForm.getContentPane().add(textField_2);
-		textField_2.setColumns(10);
+		emailTextField = new JTextField();
+		emailTextField.setBounds(282, 117, 156, 19);
+		librarianForm.getContentPane().add(emailTextField);
+		emailTextField.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(282, 152, 156, 19);
-		librarianForm.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
+		addressTextField = new JTextField();
+		addressTextField.setBounds(282, 152, 156, 19);
+		librarianForm.getContentPane().add(addressTextField);
+		addressTextField.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(282, 187, 156, 19);
-		librarianForm.getContentPane().add(textField_4);
-		textField_4.setColumns(10);
+		cityTextField = new JTextField();
+		cityTextField.setBounds(282, 187, 156, 19);
+		librarianForm.getContentPane().add(cityTextField);
+		cityTextField.setColumns(10);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(282, 222, 156, 19);
-		librarianForm.getContentPane().add(textField_5);
-		textField_5.setColumns(10);
+		contactTextField = new JTextField();
+		contactTextField.setBounds(282, 222, 156, 19);
+		librarianForm.getContentPane().add(contactTextField);
+		contactTextField.setColumns(10);
 		
-		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(174, 49, 70, 15);
-		librarianForm.getContentPane().add(lblNome);
+		JLabel lblName = new JLabel("Nome");
+		lblName.setBounds(174, 49, 70, 15);
+		librarianForm.getContentPane().add(lblName);
 		
-		JLabel lblSenha = new JLabel("Senha");
-		lblSenha.setBounds(174, 84, 70, 15);
-		librarianForm.getContentPane().add(lblSenha);
+		JLabel lblPassword = new JLabel("Senha");
+		lblPassword.setBounds(174, 84, 70, 15);
+		librarianForm.getContentPane().add(lblPassword);
 		
 		JLabel lblEmail = new JLabel("Email");
 		lblEmail.setBounds(174, 119, 70, 15);
 		librarianForm.getContentPane().add(lblEmail);
 		
-		JLabel lblNewLabel = new JLabel("Endereço");
-		lblNewLabel.setBounds(174, 154, 70, 15);
-		librarianForm.getContentPane().add(lblNewLabel);
+		JLabel lblAddress = new JLabel("Endereço");
+		lblAddress.setBounds(174, 154, 70, 15);
+		librarianForm.getContentPane().add(lblAddress);
 		
-		JLabel lblCidade = new JLabel("Cidade");
-		lblCidade.setBounds(174, 189, 70, 15);
-		librarianForm.getContentPane().add(lblCidade);
+		JLabel lblCity = new JLabel("Cidade");
+		lblCity.setBounds(174, 189, 70, 15);
+		librarianForm.getContentPane().add(lblCity);
 		
-		JLabel lblNewLabel_1 = new JLabel("Contato");
-		lblNewLabel_1.setBounds(174, 224, 70, 15);
-		librarianForm.getContentPane().add(lblNewLabel_1);
+		JLabel lblContact = new JLabel("Contato");
+		lblContact.setBounds(174, 224, 70, 15);
+		librarianForm.getContentPane().add(lblContact);
 		
-		JButton btnAdicionar = new JButton("Adicionar");
-		btnAdicionar.setBounds(282, 270, 117, 35);
-		librarianForm.getContentPane().add(btnAdicionar);
+		JButton btnAdd = new JButton("Adicionar");
+		btnAdd.setBounds(282, 270, 117, 35);
+		btnAdd.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String name = nameTextField.getText();
+				String password = passwordTextField.getText();
+				String email = emailTextField.getText();
+				String address = addressTextField.getText();
+				String city = cityTextField.getText();
+				String contact = contactTextField.getText();
+				
+				int status = Librarian.save(name, password, email, address, city, contact);
+				
+				if(name.equals(null) || password.equals(null) || email.equals(null) || address.equals(null) || city.equals(null) || contact.equals(null)) {
+					JOptionPane.showMessageDialog(btnAdd, LibrarianForm.this, "Todos os campos precisam ser preenchidos!",  status, null);
+					nameTextField.setText("");
+					passwordTextField.setText("");
+					emailTextField.setText("");
+					addressTextField.setText("");
+					cityTextField.setText("");
+					contactTextField.setText("");
+				}else {
+				if(status > 0) {
+					JOptionPane.showMessageDialog(btnAdd, LibrarianForm.this,"Bibliotecário adicionado com sucesso!", status, null);
+						new AdminSection();
+						AdminSection.main(null);
+						librarianForm.dispose();
+				}else {
+					JOptionPane.showMessageDialog(btnAdd, LibrarianForm.this,"Desculpe!, Não foi possível salvar este bibliotecário!", status, null);
+				}
+			}
+		}
+		});
+		librarianForm.getContentPane().add(btnAdd);
 		
-		JButton btnVoltar = new JButton("Voltar");
-		btnVoltar.setBounds(282, 391, 100, 25);
-		librarianForm.getContentPane().add(btnVoltar);
-		btnVoltar.addActionListener(new ActionListener() {
+		JButton btnBack = new JButton("Voltar");
+		btnBack.setBounds(282, 391, 100, 25);
+		librarianForm.getContentPane().add(btnBack);
+		btnBack.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				new AdminSection();
