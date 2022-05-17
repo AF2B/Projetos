9b4c80a4a47afa5dbce3354f53feac608d8b4b00
André Filipe <dev_andre.filipe@outlook.com>
2022-05-17 04:15:46 -0300

    Update AdminSection.java

--- Biblioteca-Swing/Biblioteca/src/components/adminSection/AdminSection.java
+++ Biblioteca-Swing/Biblioteca/src/components/adminSection/AdminSection.java
@@ -1,8 +1,16 @@
 package components.adminSection;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+
+import components.addLibrarianForm.LibrarianForm;
+import components.checkLibrarian.CheckLibrarian;
+import components.deleteLibrarian.DeleteLibrarian;
+import components.index.Index;
+
 import javax.swing.JButton;
 
 public class AdminSection {
@@ -42,21 +50,54 @@ public class AdminSection {
 		adminSection.setBounds(350, 350, 700, 500);
 		adminSection.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JButton btnNewButton_2 = new JButton("Adicionar Bibliotecário");
-		btnNewButton_2.setBounds(247, 74, 203, 25);
-		adminSection.getContentPane().add(btnNewButton_2);
+		JButton btnAddLibrarian = new JButton("Adicionar Bibliotecário");
+		btnAddLibrarian.setBounds(247, 74, 203, 25);
+		adminSection.getContentPane().add(btnAddLibrarian);
+		btnAddLibrarian.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new LibrarianForm();
+				LibrarianForm.main(null);
+				adminSection.dispose();
+			}
+		});
 		
-		JButton btnNewButton_3 = new JButton("Checar Bibliotecário");
-		btnNewButton_3.setBounds(247, 139, 203, 25);
-		adminSection.getContentPane().add(btnNewButton_3);
+		JButton btnCheckLibrarian = new JButton("Checar Bibliotecário");
+		btnCheckLibrarian.setBounds(247, 139, 203, 25);
+		btnCheckLibrarian.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new CheckLibrarian();
+				CheckLibrarian.main(null);
+				adminSection.dispose();
+			}
+		});
+		adminSection.getContentPane().add(btnCheckLibrarian);
 		
-		JButton btnNewButton = new JButton("Deletar Bibliotecário");
-		btnNewButton.setBounds(247, 202, 203, 25);
-		adminSection.getContentPane().add(btnNewButton);
+		JButton btnDeleteLibrarian = new JButton("Deletar Bibliotecário");
+		btnDeleteLibrarian.setBounds(247, 202, 203, 25);
+		btnDeleteLibrarian.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new DeleteLibrarian();
+				DeleteLibrarian.main(null);
+				adminSection.dispose();
+			}
+		});
+		adminSection.getContentPane().add(btnDeleteLibrarian);
 		
-		JButton btnNewButton_1 = new JButton("Sair");
-		btnNewButton_1.setBounds(290, 306, 100, 25);
-		adminSection.getContentPane().add(btnNewButton_1);
+		JButton btnExit = new JButton("Sair");
+		btnExit.setBounds(290, 306, 100, 25);
+		btnExit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new Index();
+				Index.main(null);
+				adminSection.dispose();
+			}
+		});
+		adminSection.getContentPane().add(btnExit);
+		
 	}
 
 }
