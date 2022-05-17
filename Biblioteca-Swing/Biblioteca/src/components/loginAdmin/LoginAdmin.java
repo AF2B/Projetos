00f97a027ce7117bb297e11c61b425671a8b4725
André Filipe <dev_andre.filipe@outlook.com>
2022-05-17 04:15:15 -0300

    Update LoginAdmin.java

--- Biblioteca-Swing/Biblioteca/src/components/loginAdmin/LoginAdmin.java
+++ Biblioteca-Swing/Biblioteca/src/components/loginAdmin/LoginAdmin.java
@@ -3,17 +3,24 @@ package components.loginAdmin;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+
+import components.adminSection.AdminSection;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
 
 public class LoginAdmin {
 
 	private JFrame loginAdmin;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField nameTextField;
+	private JTextField passwordTextField;
 
 	/**
 	 * Launch the application.
@@ -48,34 +55,57 @@ public class LoginAdmin {
 		loginAdmin.getContentPane().setBackground(new Color(245, 245, 245));
 		loginAdmin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		loginAdmin.setBounds(350, 350, 700, 500);
-		
-		JButton btnEntrar = new JButton("Entrar");
-		btnEntrar.setBounds(288, 333, 117, 30);
-		loginAdmin.getContentPane().add(btnEntrar);
-		
-		textField = new JTextField();
-		textField.setBounds(291, 109, 120, 19);
-		loginAdmin.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setBounds(291, 192, 120, 19);
-		loginAdmin.getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Nome");
-		lblNewLabel.setBounds(152, 111, 70, 15);
-		loginAdmin.getContentPane().add(lblNewLabel);
-		
-		JLabel lblSenha = new JLabel("Senha");
-		lblSenha.setBounds(152, 194, 70, 15);
-		loginAdmin.getContentPane().add(lblSenha);
-		
-		JLabel lblAdministrador = new JLabel("Administrador");
-		lblAdministrador.setBounds(274, 22, 153, 15);
-		lblAdministrador.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 20));
-		loginAdmin.getContentPane().add(lblAdministrador);
-		
-		
+
+		JButton btnLogin = new JButton("Entrar");
+		btnLogin.setBounds(288, 333, 117, 30);
+		btnLogin.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String name = nameTextField.getText();
+				String password = String.valueOf(passwordTextField.getText());
+				if (name.equals("admin") && password.equals("admin123")) {
+					new AdminSection();
+					AdminSection.main(null);
+					loginAdmin.dispose();
+				} else if(name.equals("") && password.equals("")){
+					JOptionPane.showMessageDialog(btnLogin, LoginAdmin.this, 
+							"Desculpe, O Campo usuário e senha não podem ser vázios!",
+							JOptionPane.ERROR_MESSAGE);
+					nameTextField.setText("");
+					passwordTextField.setText("");
+				}else {
+					JOptionPane.showMessageDialog(btnLogin, LoginAdmin.this, 
+							"Desculpe, Usuário ou Senha invalido!",
+							JOptionPane.ERROR_MESSAGE);
+					nameTextField.setText("");
+					passwordTextField.setText("");
+				}
+			}
+		});
+		loginAdmin.getContentPane().add(btnLogin);
+
+		nameTextField = new JTextField();
+		nameTextField.setBounds(291, 109, 120, 19);
+		loginAdmin.getContentPane().add(nameTextField);
+		nameTextField.setColumns(10);
+
+		passwordTextField = new JPasswordField();
+		passwordTextField.setBounds(291, 192, 120, 19);
+		loginAdmin.getContentPane().add(passwordTextField);
+		passwordTextField.setColumns(10);
+
+		JLabel lblName = new JLabel("Nome");
+		lblName.setBounds(152, 111, 70, 15);
+		loginAdmin.getContentPane().add(lblName);
+
+		JLabel lblPassword = new JLabel("Senha");
+		lblPassword.setBounds(152, 194, 70, 15);
+		loginAdmin.getContentPane().add(lblPassword);
+
+		JLabel lblAdmin = new JLabel("Administrador");
+		lblAdmin.setBounds(274, 22, 153, 15);
+		lblAdmin.setFont(new Font("DejaVu Sans Condensed", Font.BOLD, 20));
+		loginAdmin.getContentPane().add(lblAdmin);
+
 	}
 }
