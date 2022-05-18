a0b0c2f88bc57487dafa55677d2bf26b13ee4912
André Filipe <dev_andre.filipe@outlook.com>
2022-05-18 02:15:32 -0300

    Update CheckLibrarian.java

--- Biblioteca-Swing/Biblioteca/src/components/checkLibrarian/CheckLibrarian.java
+++ Biblioteca-Swing/Biblioteca/src/components/checkLibrarian/CheckLibrarian.java
@@ -1,10 +1,21 @@
 package components.checkLibrarian;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 import javax.swing.JFrame;
 import javax.swing.JTable;
+
+import components.adminSection.AdminSection;
+import repository.DB;
+
 import javax.swing.JScrollPane;
+import javax.swing.JButton;
 
 public class CheckLibrarian {
 
@@ -45,17 +56,55 @@ public class CheckLibrarian {
 		checkLibrarian.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		String data[][] = null;
 		String column[] = null;
-		
+
+		try {
+			Connection con = DB.getConnection();
+			PreparedStatement ps = con.prepareStatement("select * from librarian", ResultSet.TYPE_SCROLL_SENSITIVE,
+					ResultSet.CONCUR_UPDATABLE);
+			ResultSet rs = ps.executeQuery();
+
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int cols = rsmd.getColumnCount();
+			column = new String[cols];
+			for (int i = 1; i <= cols; i++) {
+				column[i - 1] = rsmd.getColumnName(i);
+			}
+
+			rs.last();
+			int rows = rs.getRow();
+			rs.beforeFirst();
+
+			data = new String[rows][cols];
+			int count = 0;
+			while (rs.next()) {
+				for (int i = 1; i <= cols; i++) {
+					data[count][i - 1] = rs.getString(i);
+				}
+				count++;
+			}
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
 		table = new JTable(data, column);
 		checkLibrarian.getContentPane().add(table);
-		
+
 		JScrollPane scrollPane = new JScrollPane(table);
 		scrollPane.setBounds(12, 12, 676, 324);
 		checkLibrarian.getContentPane().add(scrollPane);
 		
-		
-		
-		
-		
+		JButton btnBack = new JButton("Voltar");
+		btnBack.setBounds(287, 395, 100, 30);
+		btnBack.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new AdminSection();
+				AdminSection.main(null);
+				checkLibrarian.dispose();
+			}
+		});
+		checkLibrarian.getContentPane().add(btnBack);
+
 	}
 }
