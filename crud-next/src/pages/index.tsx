37224665cfaf4e2f97f83d9dc21cb2cf7f0b09ba
André Filipe <dev_andre.filipe@outlook.com>
2022-05-19 05:53:04 -0300

    Update index.tsx

--- crud-next/src/pages/index.tsx
+++ crud-next/src/pages/index.tsx
@@ -1,48 +1,12 @@
-import { TableHTMLAttributes, useEffect, useState } from "react";
 import Button from "../components/Button";
 import Form from "../components/Form";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
-import Client from "../core/Client";
-import ClientRepository from "../core/ClientRepository";
-import CollectionClient from "../firebase/db/CollectionClient";
+import useClients from "../hooks/useClients";
 
 export default function Home() {
 
-    const repo: ClientRepository = new CollectionClient()
-
-    const [visible, setVisible] = useState<"table" | "form">("table")
-    const [clients, setClients] = useState<Client[]>([])
-    const [client, setClient] = useState<Client>(Client.clientNull())
-
-    useEffect(getAll, [])
-
-    function getAll() {
-        repo.readAll().then(clients => {
-            setClients(clients)
-            setVisible("table")
-        })
-    }
-
-    function selectedClient(client: Client) {
-        setClient(client)
-        setVisible("form")
-    }
-
-    function newClient() {
-        setClient(Client.clientNull())
-        setVisible("form")
-    }
-
-    async function deleteClient(client: Client) {
-        await repo.delete(client)
-        getAll()
-    }
-
-    async function saveClient(client: Client) {
-        await repo.save(client)
-        getAll()
-    }
+    const { newClient, selectedClient, saveClient, deleteClient, client, clients, visibleTable, viewTable } = useClients()
 
     return (
         <div className="flex 
@@ -53,7 +17,7 @@ export default function Home() {
         from-black to bg-gray-500">
 
             <Layout title="Cadastro">
-                {visible === "table" ? (
+                {visibleTable ? (
                     <>
                         <div className="flex justify-end">
                             <Button onClick={newClient}
@@ -65,7 +29,7 @@ export default function Home() {
                     </>
                 ) : (
                     <Form client={client}
-                        cancel={() => setVisible("table")}
+                        cancel={viewTable}
                         clientChange={saveClient} />
                 )}
             </Layout>
