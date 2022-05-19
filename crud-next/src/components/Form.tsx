78f77995e370972296638beb0ee0b2653fdad6b4
André Filipe <dev_andre.filipe@outlook.com>
2022-05-19 05:52:48 -0300

    Update Form.tsx

--- crud-next/src/components/Form.tsx
+++ crud-next/src/components/Form.tsx
@@ -17,11 +17,11 @@ export default function Form(props: FormProps) {
     return (
         <div>
             {id ? (
-                <Input text="ID" value={id} justRead className="mb-4" />
+                <Input text="ID: " value={id} justRead className="mb-4 ml-7" />
             ) : false}
 
-            <Input text="name" value={name} valueChange={setName} className="mb-4" />
-            <Input text="age" value={age} type="number" valueChange={setAge} />
+            <Input text="Nome: " value={name} valueChange={setName} className="mb-4" />
+            <Input text="Idade: " value={age} type="number" valueChange={setAge} className="ml-1"/>
             <div className="flex justify-end mt-3">
                 <Button onClick={() => props.clientChange?.(new Client(name, age, id))} className="mr-2">{id ? "Alterar" : "Salvar"}</Button>
                 <Button onClick={props.cancel} className="">Cancelar</Button>
