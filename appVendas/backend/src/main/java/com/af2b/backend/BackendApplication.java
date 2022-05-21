240c040f0cc3a68f13ede7d0f7d65a80725d11b5
André Filipe <dev_andre.filipe@outlook.com>
2022-05-21 19:30:39 -0300

    operation to save categories on db

--- appVendas/backend/src/main/java/com/af2b/backend/BackendApplication.java
+++ appVendas/backend/src/main/java/com/af2b/backend/BackendApplication.java
@@ -1,13 +1,31 @@
 package com.af2b.backend;
 
+import java.util.Arrays;
+
+import com.af2b.backend.Repository.CategoryRepository;
+import com.af2b.backend.domain.Category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoryRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category category1 = new Category(null, "Informática");
+		Category category2 = new Category(null, "Escritório");
+
+		repository.saveAll(Arrays.asList(category1, category2));
 	}
 
 }
