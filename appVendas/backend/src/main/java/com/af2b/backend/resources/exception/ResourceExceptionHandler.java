b9bbc03b24a828604f6d0b0f0487291bc06e06da
André Filipe <dev_andre.filipe@outlook.com>
2022-05-21 22:01:15 -0300

    Create ResourceExceptionHandler.java

---
+++ appVendas/backend/src/main/java/com/af2b/backend/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.af2b.backend.resources.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.af2b.backend.services.Exceptions.ObjectNotFoundException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+    
+}
