996a084ff1b44a6dfb157897b75911d730ffd694
André Filipe <dev_andre.filipe@outlook.com>
2022-05-21 22:01:01 -0300

    bug solved

--- appVendas/backend/src/main/java/com/af2b/backend/domain/Category.java
+++ appVendas/backend/src/main/java/com/af2b/backend/domain/Category.java
@@ -4,13 +4,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name = "Category")
@@ -19,10 +20,10 @@ public class Category implements Serializable{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID")
     private Integer id;
     private String name;
 
+    @JsonManagedReference
     @ManyToMany(mappedBy = "categories")
     private List<Product> products = new ArrayList<>();
 
