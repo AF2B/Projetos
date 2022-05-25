d775e04ea8ebc912d321ef6c50832e933577b3fc
André Filipe <dev_andre.filipe@outlook.com>
2022-05-24 22:36:49 -0300

    Update CategoryService.java

--- appVendas/backend/src/main/java/com/af2b/backend/services/CategoryService.java
+++ appVendas/backend/src/main/java/com/af2b/backend/services/CategoryService.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 import com.af2b.backend.Repository.CategoryRepository;
 import com.af2b.backend.domain.Category;
+import com.af2b.backend.services.Exceptions.DataIntegrityViolation;
 import com.af2b.backend.services.Exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,8 +23,7 @@ public class CategoryService {
     public Category find(Integer id) {
         Optional<Category> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
-            "Objeto não encontrado! id: " + id + ", Tipo: " + Category.class.getName()
-        ));
+                "Objeto não encontrado! id: " + id + ", Tipo: " + Category.class.getName()));
     }
 
     public Category insert(Category obj) {
@@ -33,5 +34,14 @@ public class CategoryService {
     public Category update(Category obj) {
         find(obj.getId());
         return repository.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityViolation("Não é possível excluir uma categoria que possui produtos!");
+        }
     }
 }
