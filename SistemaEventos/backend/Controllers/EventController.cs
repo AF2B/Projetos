5f8f9c3a5878e4b82c18b2968aec61edc2eb1289
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-05-27 11:08:21 -0300

    binary changes

--- SistemaEventos/backend/Controllers/EventController.cs
+++ SistemaEventos/backend/Controllers/EventController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using backend.Data;
 using backend.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,42 +11,25 @@ namespace backend.Controllers
     [Route("api/[controller]")]
     public class EventController : ControllerBase
     {
-        public IEnumerable<Event> _event = new Event[] {
-            new Event() {
-                EventId = 1,
-                Theme = "Angular é show!" ,
-                Local = "Pernambuco",
-                Lote = "Primeiro",
-                LengthMax = 250,
-                EventDate = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImageURL = "Photo.png"
-            },
-            new Event() {
-                EventId = 2,
-                Theme = "React também!" ,
-                Local = "Pernambuco",
-                Lote = "Segundo",
-                LengthMax = 550,
-                EventDate = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImageURL = "Photo2.png"
-            }
-        };
+        
+        private readonly DataContext context;
 
-        public EventController()
+        public EventController(DataContext context)
         {
+            this.context = context;
 
         }
 
         [HttpGet]
         public IEnumerable<Event> Get()
         {
-            return _event;
+            return context.Events;
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Event> GetById(int id)
         {
-            return _event.Where(_event => _event.EventId == id);
+            return context.Events.Where(_event => _event.EventId == id);
         }
     }
 }
