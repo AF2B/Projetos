5ab562b9d9f98784211ec9f491ad8f3569c447be
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-05-27 14:11:12 -0300

    frontend project created Angular 13

---
+++ SistemaEventos/frontend/src/app/panelist/panelist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panelist',
+  templateUrl: './panelist.component.html',
+  styleUrls: ['./panelist.component.scss']
+})
+export class PanelistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
