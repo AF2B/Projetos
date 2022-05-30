27461eaf09611bd038b9536f6d27a5ca54998b1f
André Filipe <81587964+AF2B@users.noreply.github.com>
2022-05-30 10:22:50 -0300

    update events component

--- SistemaEventos/frontend/src/app/app.module.ts
+++ SistemaEventos/frontend/src/app/app.module.ts
@@ -3,23 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
 import { PanelistComponent } from './panelist/panelist.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
       EventsComponent,
-      PanelistComponent
+      PanelistComponent,
+      NavbarComponent
    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
